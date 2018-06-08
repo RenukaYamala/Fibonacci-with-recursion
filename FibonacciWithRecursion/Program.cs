0dd00dbd1ef594d5f93fdc9190b4e9e91d62628a
RenukaYamala <renuka.yamala@neudesic.com>
2018-06-08 11:21:06 +0530

    First Commit

---
+++ FibonacciWithRecursion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciWithRecursion
+{
+    class Program
+    {
+       
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the Limit");
+            int limit =int.Parse(Console.ReadLine());
+           // fibonacci(limit);
+            Console.WriteLine(" " + fibonacci(limit));
+        }
+        public static int fibonacci(int number)
+        {
+
+            if (number == 0)
+                return 0;
+            else if (number == 1)
+                return 1;
+            else
+            {
+                return fibonacci(number - 2) + fibonacci(number - 1);
+            }
+
+        }
+
+    }
+}
